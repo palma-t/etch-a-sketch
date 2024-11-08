6d4f5daa4fa788b452b8bc829346f454ca2e5d10
Palma <184235218+palma-t@users.noreply.github.com>
2024-11-08 11:43:37 +0100

    Add possibility to change several times number of squares

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const grille = document.querySelector("#container");
 
-const firstDiv = document.createElement("div");
+let firstDiv = document.createElement("div");
 
 for(let i = 0; i < 256; i++){
     const square2 = document.createElement("div");
@@ -19,28 +19,28 @@ grille.appendChild(firstDiv);
 const btn = document.querySelector("#btn");
 
 btn.addEventListener("click", () => {
-    let numberasked = prompt("How many squares do you want?");
-    let squareNumber = parseInt(numberasked);
-    grille.removeChild(firstDiv);
-    const newDiv = document.createElement("div");
-    for(let i = 0; i < (squareNumber*squareNumber); i++){
-        const square3 = document.createElement("div");
-        square3.style.backgroundColor = "";
-        let squareHeight = 320/squareNumber;
-        square3.style.height = `${squareHeight}px`;
-        console.log(square3.style.height = 320/20);
-        square3.style.width = `${squareHeight}px`;
-        square3.className = "square";
-        square3.addEventListener("mouseover", () => {
-            square3.style.backgroundColor = "red";
-        });
-        newDiv.appendChild(square3);
+    let numberAsked = prompt("How many squares do you want? (max 100)");
+    let squareNumber = parseInt(numberAsked);
+    if (squareNumber > 100){
+        alert("Please, choose a number of squares under 100.");
+    } else if(isNaN(squareNumber)){
+        alert("Please, write your number with numbers.");
+    } else{
+        grille.removeChild(firstDiv);
+        firstDiv = document.createElement("div");
+        for(let i = 0; i < (squareNumber*squareNumber); i++){
+            const square3 = document.createElement("div");
+            square3.style.backgroundColor = "";
+            let squareHeight = 320/squareNumber;
+            square3.style.height = `${squareHeight}px`;
+            console.log(square3.style.height = 320/20);
+            square3.style.width = `${squareHeight}px`;
+            square3.className = "square";
+            square3.addEventListener("mouseover", () => {
+                square3.style.backgroundColor = "red";
+            });
+            firstDiv.appendChild(square3);
+        }
+        grille.appendChild(firstDiv);
     }
-    grille.appendChild(newDiv);
-})
-
-/* reste à faire : 
-- recommencer la manip, que ça fonctionne autant de fois qu'on veut
-=> changer les noms des div pour gérer ça
-- gérer le nombre max de squares
-- la suite de l'exo ! */
+});
